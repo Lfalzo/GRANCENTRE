e8d43916d17b8391294e4387fc771e6b4773b961
GerardVillaret <gerard.villaretv@educem.net>
2022-10-31 09:05:41 +0100

    gerard

--- app/src/main/java/com/example/proyectoandroid/MainActivity.java
+++ app/src/main/java/com/example/proyectoandroid/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.proyectoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button hotels = findViewById(R.id.hotels);
+        hotels.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button b = (Button) view;
+
+        if (b.getId() == R.id.hotels)
+        {
+            Intent hotels =new Intent(this, activity_hotels.class);
+
+            startActivity(hotels);
+        }
     }
 }
--- app/src/main/java/com/example/proyectoandroid/activity_hotels.java
+++ app/src/main/java/com/example/proyectoandroid/activity_hotels.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class hotels extends AppCompatActivity {
+public class activity_hotels extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
