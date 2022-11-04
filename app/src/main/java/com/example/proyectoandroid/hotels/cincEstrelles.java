1b4cbd97c02e265e7970641637282003073aff17
GerardVillaret <gerard.villaretv@educem.net>
2022-11-04 10:02:02 +0100

    gerard

--- app/src/main/java/com/example/proyectoandroid/hotels/cincEstrelles.java
+++ app/src/main/java/com/example/proyectoandroid/hotels/cincEstrelles.java
@@ -45,6 +45,7 @@ public class cincEstrelles extends Fragment {
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
+
         return fragment;
     }
 
